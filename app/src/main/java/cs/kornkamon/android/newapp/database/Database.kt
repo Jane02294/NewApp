604862a2b3c188cac6817ec3ce28ecfea374672d
Jane02294 <46182833+Jane02294@users.noreply.github.com>
2020-08-27 16:12:15 +0700

    Step 17 Create the Database class

---
+++ app/src/main/java/cs/kornkamon/android/newapp/database/Database.kt
@@ -0,0 +1,33 @@
+package cs.kornkamon.android.newapp.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [Contact::class], version = 1, exportSchema = false)
+abstract class Database : RoomDatabase(){
+    abstract val databaseDao: DatabaseDAO
+
+    companion object{
+        @Volatile
+        private var INSTANCE: cs.kornkamon.android.newapp.database.Database? = null
+        fun getInstance(context: Context): cs.kornkamon.android.newapp.database.Database {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        cs.kornkamon.android.newapp.database.Database::class.java,
+                        "database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
